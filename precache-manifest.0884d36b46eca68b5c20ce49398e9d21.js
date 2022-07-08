6927020ce533e7ac2f15a7087f667bf14af8e7f7
Shoab <shoab542@gmail.com>
2022-07-08 20:18:01 +0600

    Updates

---
+++ precache-manifest.0884d36b46eca68b5c20ce49398e9d21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde2d77462a968988d19f451228a2d77",
+    "url": "/myfirebase/index.html"
+  },
+  {
+    "revision": "3f98138524272ee1c3e4",
+    "url": "/myfirebase/static/css/main.4bdf0658.chunk.css"
+  },
+  {
+    "revision": "9be2568482fc1b1ae314",
+    "url": "/myfirebase/static/js/2.6426c65b.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/myfirebase/static/js/2.6426c65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f98138524272ee1c3e4",
+    "url": "/myfirebase/static/js/main.94195c90.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/myfirebase/static/js/main.94195c90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f533a68fbbd408440aa8",
+    "url": "/myfirebase/static/js/runtime-main.746b018a.js"
+  }
+]);
